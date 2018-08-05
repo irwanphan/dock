7f8049474f44d8dfeaf646cfb9af6bbd08593124
@irwanphan <irwanphan@gmail.com>
2018-08-05 19:17:22 +0700

    add a cllose button to tab header, close tab js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,18 +3,18 @@ $(document).ready(function () {
 	$( '.nav-item' ).click( function(){
 		var which = $(this).data('val');
 		if ( !$('.screenout').hasClass('show') )
-			$('.screenout').addClass('show')
+			$('.screenout').addClass('show');
 		if ( $('#'+which).hasClass( 'show' ) )
-			$('#'+which).removeClass( 'show' )
+			$('#'+which).removeClass( 'show' );
 		else
 			{
-				$('.popdown').removeClass( 'show' )
-				$('#'+which).toggleClass( 'show' )
+				$('.popdown').removeClass( 'show' );
+				$('#'+which).toggleClass( 'show' );
 			}
 	});
 	$( '.screenout' ).click( function(){
-		$('.popdown').removeClass( 'show' )
-		$('.screenout').removeClass( 'show' )
+		$('.popdown').removeClass( 'show' );
+		$('.screenout').removeClass( 'show' );
 	});
 
 	// tab option
@@ -22,21 +22,39 @@ $(document).ready(function () {
 		var which = $(this).data('target');
 		if ( !$('#'+which).hasClass( 'show' ) )
 		{
-			$( '.tab-header li' ).removeClass( 'selected' )
-			$(this).addClass( 'selected' )
-			$( '.tab-content .tab' ).removeClass( 'show' )
-			$('#'+which).addClass( 'show' )
+			$( '.tab-header li' ).removeClass( 'selected' );
+			$(this).addClass( 'selected' );
+			$( '.tab-content > .tab' ).removeClass( 'show' );
+			$('#'+which).addClass( 'show' );
 		}
 	});
+
+	// add a close button to every tab header
+	$( '.tab-header li' ).append( '<i>cancel</i>' );
+
+	// close a tab, if the tab was selected, select previous tab
+	$( '.tab-header li > i' ).click( function(){
+		var which = $(this).parent().data('target');
+		if ( $('#'+which).hasClass( 'show' ) ) {
+			$(this).parent().prev().addClass('selected');
+			var next = $(this).parent().siblings('.selected').data('target');
+			$('#'+next).addClass( 'show' );
+		}
+		$(this).parent().remove();
+		$( '.tab-content > .tab#'+which).remove();
+	});
+
 	// tab sub option
 	$( '.tab-sub-header li' ).click( function(){
+		var divid = $(this).parent().parent().parent().attr('id');
 		var which = $(this).data('target');
 		if ( !$('#'+which).hasClass( 'show' ) )
 		{
-			$( '.tab-sub-header li' ).removeClass( 'selected' )
-			$(this).addClass( 'selected' )
-			$( '.tab-sub-content .tab' ).removeClass( 'show' )
-			$('#'+which).addClass( 'show' )
+			$( '#'+divid+' .tab-sub-header li' ).removeClass( 'selected' );
+			$(this).addClass( 'selected' );
+			$( '#'+divid+' .tab-sub-content .tab' ).removeClass( 'show' );
+			$('#'+which).addClass( 'show' );
 		}
 	});
+
 });
